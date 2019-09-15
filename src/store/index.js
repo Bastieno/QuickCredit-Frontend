cb8ffeadaf0091c72f8aeec8a4a667c9be0e1224
Bastieno <francis.nduamaka@gmail.com>
2019-09-15 03:34:36 +0100

    chore(setup): setup redux store
    - create redux structure
    
    [Finishes #168495534]

---
+++ src/store/index.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-underscore-dangle */
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from '../reducers';
+
+const initialState = {};
+
+const middleware = [thunk];
+
+const enhancers = [];
+
+const isDev = process.env.NODE_ENV === 'development';
+
+if (isDev && typeof window !== 'undefined' && window.window.__REDUX_DEVTOOLS_EXTENSION__) {
+  enhancers.push(window.window.__REDUX_DEVTOOLS_EXTENSION__());
+}
+
+const store = createStore(
+  rootReducer,
+  initialState,
+  compose(
+    applyMiddleware(...middleware),
+    ...enhancers,
+  ),
+);
+
+export default store;
